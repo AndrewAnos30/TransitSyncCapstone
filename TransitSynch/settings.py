deda8244ca75ba57c7baec3c668617b6269c06f2
Andrew Anos <96986357+AndrewAnos30@users.noreply.github.com>
2023-11-28 04:09:04 +0800

    huhu

--- TransitSynch/settings.py
+++ TransitSynch/settings.py
@@ -83,9 +83,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'TransitSynch.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 import dj_database_url 
 DATABASES = {
    'default':  dj_database_url.parse('postgres://transitsync_user:LW1f9iVJ6kw46SWefOFm75o5PcE1wfd9@dpg-clhfill8td7s73bo8cfg-a.singapore-postgres.render.com/transitsync')
@@ -123,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
 
